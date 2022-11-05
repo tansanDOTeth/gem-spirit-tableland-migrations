53e1e46f1f912dc2ee6b29a24f622231fd8e4180
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2022-11-05 22:49:10 +0900

    feat: add insert stages

---
+++ insert-stages.js
@@ -0,0 +1,22 @@
+import { getTableArtifact, insertDifference } from './util.js';
+
+import { tableland } from "./tableland.js";
+
+const FIRST_GEN = 1; // KIPPAN 0001
+const data = [
+  [FIRST_GEN, null], // Stage 0 - ex, Gem
+  [FIRST_GEN, 1], // Stage 1 - ex, Ghost
+  [FIRST_GEN, 2], // Stage 2 - ex, Choku
+  [FIRST_GEN, 3], // Stage 3 - ex, Lion
+];
+
+const tableArtifact = await getTableArtifact('Stages');
+const { name: tableName } = tableArtifact;
+const result = await tableland.read(`SELECT * FROM ${tableName};`);
+await insertDifference(tableName, result.rows, ['generation_id', 'stage_parent_id'], data)
+const newResult = await tableland.read(`SELECT * FROM ${tableName};`);
+console.log("Latest Tableland Result",)
+newResult.rows.forEach((row) => console.log("\t", row))
+
+// Example of a delete
+// const removeRes = await tableland.write(`DELETE FROM ${tableName} WHERE id = 0;`);
