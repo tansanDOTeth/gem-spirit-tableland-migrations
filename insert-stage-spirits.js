e5be121bbed48597127da55c6b34501a179678cc
tansan.eth <92072158+tansanDOTeth@users.noreply.github.com>
2022-11-07 15:33:01 +0900

    feat: add insert spirit stages

---
+++ insert-stage-spirits.js
@@ -0,0 +1,26 @@
+import { getTableArtifact, insertDifference } from './util.js';
+
+import { tableland } from "./tableland.js";
+
+const STAGE_0_ID = 1;
+const STAGE_1_ID = 2;
+const STAGE_2_ID = 3;
+const STAGE_3_ID = 4;
+
+const data = [
+  [STAGE_0_ID, 1, null],
+  [STAGE_1_ID, 2, 1],
+  [STAGE_2_ID, 3, 2],
+  [STAGE_3_ID, 4, 3],
+];
+
+const tableArtifact = await getTableArtifact('StageSpirits');
+const { name: tableName } = tableArtifact;
+const result = await tableland.read(`SELECT * FROM ${tableName};`);
+await insertDifference(tableName, result.rows, ['stage_id', 'spirit_id', 'stage_spirit_parent_id'], data)
+const newResult = await tableland.read(`SELECT * FROM ${tableName};`);
+console.log("Latest Tableland Result")
+newResult.rows.forEach((row) => console.log("\t", row))
+
+// Example of a delete
+// const removeRes = await tableland.write(`DELETE FROM ${tableName} WHERE id = 0;`);
